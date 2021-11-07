b8798ad11db7689c667501fc7f09b425a0f5fc15
KANGHO <ariaskkh@gmail.com>
2021-11-07 20:31:00 +0900

    21/11/07 백준

---
+++ 백준/11720_숫자의 합.py
@@ -0,0 +1,13 @@
+# import sys
+# input = sys.stdin.readline
+
+n = int(input())
+# x = list(input().strip())
+# sum = 0
+
+# for num in x:
+#     sum += int(num)
+# print(sum)
+    
+x = sum(map(int,input()))
+print(x)
