4b708686948672faf8ff36d82d615e327de77321
KANGHO <ariaskkh@gmail.com>
2021-11-13 02:24:09 +0900

    21/11/13 대칭 차집합

--- 백준/1269_대칭 차집합.py
+++ 백준/1269_대칭 차집합.py
@@ -1,23 +1,31 @@
-# import sys
-# input = sys.stdin.readline
+import sys
+input = sys.stdin.readline
 
-# x, y = map(int, input().split())
+x, y = map(int, input().split())
 
-# A = list(map(int,input().split()))
-# B = list(map(int,input().split()))
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
 
-# cnt = 0
-# if x > y:
-#     for elem in A:
-#         if elem in B:
-#             cnt += 1
+cnt = 0
+if x > y:
+    for elem in A:
+        if elem in B:
+            cnt += 1
+else:
+    for elem in B:
+        if elem in A:
+            cnt += 1
+print(x+y-cnt*2)
 
-# else:
-#     for elem in B:
-#         if elem in A:
-#             cnt += 1
-# print(x+y-cnt*2)
 
-a = [2,1,3]
-a.sort()
-print(a)
+###############
+
+import sys
+input = sys.stdin.readline
+
+x, y = map(int, input().split())
+
+A = set(map(int,input().split()))
+B = set(map(int,input().split()))
+
+print(len(A-B) + len(B-A))
