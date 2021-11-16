676fe724b1691708bccc283d132f0b7d2d18123f
KANGHO <ariaskkh@gmail.com>
2021-11-16 12:40:54 +0900

    21/11/16 에디터

--- 백준/1406_에디터.py
+++ 백준/1406_에디터.py
@@ -1,32 +1,60 @@
+# import sys
+# input = sys.stdin.readline
+
+# string = list(input().strip())
+# cursor = len(string)
+# string.insert(0, '+')
+# print(string)
+
+# M = int(input().strip())
+# p = 0
+
+# for _ in range(M):
+#     x = input().split()
+#     # print(x)
+#     if x[0] == 'L':
+#         if cursor != 0:
+#             cursor -= 1
+#     elif x[0] == 'D':
+#         if cursor != M+p:
+#             cursor += 1
+#     elif x[0] == 'B':
+#         if cursor != 0:
+#             del string[cursor]
+#             cursor -= 1
+#             print(string, 'B')
+#     else: ## P인 경우
+#         string.insert(cursor+1, x[1])
+#         cursor += 1
+#         p += 1
+#         print(string, 'P')
+
+# print(''.join(string[1:]))
+
+
+##################################################################
+##스택을 이용한 풀이
+
 import sys
 input = sys.stdin.readline
 
-string = list(input().strip())
-i = len(string)
-string.insert(0, '+')
-print(string)
-
+stack = list(input().strip())
+# print(stack)
+stack2 = []
 M = int(input().strip())
-p = 0
 
 for _ in range(M):
     x = input().split()
-    # print(x)
-    if x[0] == 'L':
-        if i != 0:
-            i -= 1
-    elif x[0] == 'D':
-        if i != M+p:
-            i += 1
-    elif x[0] == 'B':
-        if i != 0:
-            del string[i]
-            i -= 1
-            print(string, 'B')
-    else: ## P인 경우
-        string.insert(i+1, x[1])
-        i += 1
-        p += 1
-        print(string, 'P')
-
-print(''.join(string[1:]))
+    
+    if x[0] == 'L' and len(stack) != 0:
+        stack2.append(stack.pop())
+    elif x[0] == 'D' and len(stack2) != 0:
+        stack.append(stack2.pop())
+    elif x[0] == 'B' and len(stack) != 0:
+        stack.pop()
+    elif x[0] == 'P': ## P 문자 왼쪽 추가
+        stack.append(x[1])
+for i in stack:
+    print(i, end="")
+for j in range(len(stack2)):
+    print(stack2.pop(), end="")
