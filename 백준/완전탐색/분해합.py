6ff88e579ec3d07328b33a985ad4b551c33586a7
KANGHO <ariaskkh@gmail.com>
2021-11-19 13:43:08 +0900

    21/11/19 분해합 최적화

--- 백준/완전탐색/분해합.py
+++ 백준/완전탐색/분해합.py
@@ -3,12 +3,23 @@ input = sys.stdin.readline
 
 x = input().strip()
 X = int(x)
+
 cnt = 0
-for i in range(X):
-    tmp = list(str(i))
-    if X == i + sum(map(int, tmp)):
-        print(i)
-        cnt +=1
-        break
+min_x = abs(X-len(x)*9)
+
+if X <= 18:
+    for i in range(X):
+        tmp = list(str(i))
+        if X == i + sum(map(int, tmp)):
+            print(i)
+            cnt +=1
+            break
+else:
+    for i in range(min_x,X):
+        tmp = list(str(i))
+        if X == i + sum(map(int, tmp)):
+            print(i)
+            cnt +=1
+            break
 if cnt == 0:
     print(0)
