d7bcc0e4ad51318c3879d5771d7f780c9620abef
KANGHO <ariaskkh@gmail.com>
2021-12-23 20:41:44 +0900

    21/12/23 스택_괄호

---
+++ 백준/스택/괄호.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input().strip())
+
+def solution(x):
+    
+    stack = []
+    # print(i,x,stack)
+    i = 0
+    while i < len(x):
+        if x[i] == "(":
+            stack.append('(')
+        else: # i == ")"
+            if len(stack) != 0:
+                stack.pop()
+            else:
+                return False
+        i += 1
+    
+    if len(stack) == 0:
+        return True
+    else:
+        return False
+
+for _ in range(n):
+    X = input().strip()
+    
+    if solution(X) == False:
+        print('NO')
+    else:
+        print('YES')
